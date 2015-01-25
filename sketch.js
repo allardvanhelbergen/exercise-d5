cb1f9bd63b9ce08732f0156e664f3f865df5fa05
Allard van Helbergen <allard@brandwatch.com>
2015-01-25 17:11:54 +0100

    Circle clicky thing, styled the canvas.

--- sketch.js
+++ sketch.js
@@ -1,7 +1,15 @@
 function setup() {
-  // put setup code here
+  var myCanvas = createCanvas(640,480);
+  myCanvas.parent('canvas-container');
+  myCanvas.class('my-canvas');
 }
 
 function draw() {
-  ellipse(50, 50, 80, 80)
+  if(mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+
+  ellipse(mouseX, mouseY, 80, 80);
 }
